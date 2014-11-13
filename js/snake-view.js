452f9cd034d28ffd0df0f4f6637b6378374addf3
Aaron Strick <aaronstrick@gmail.com>
2014-11-13 01:00:00 -0800

    enabled keyboard controls for snake

--- js/snake-view.js
+++ js/snake-view.js
@@ -2,23 +2,56 @@
     if (typeof Snake === "undefined") {
         window.Snake = {};
     }
-    
+
     var BOARD_SIZE = 20;
-    
+
     var View = Snake.View = function ($el) {
         this.$el = $el;
         this.body = new Snake.Body();
         this.render();
-        this.();
+        this.start();
+
+        $(window).on("keydown", this.handleKeyEvent.bind(this));
+}
+
+    View.KEYS = {
+      87: "N",
+      68: "E",
+      83: "S",
+      65: "W"
     }
-    
-    Snake.View.prototype.handleKeyPress = function(event) {
-        // if (event.keyCode == 87
+
+    Snake.View.prototype.handleKeyEvent = function(event) {
+      switch(event.keyCode) {
+        case 87:
+          this.body.turn("N")
+          break;
+        case 68:
+          this.body.turn("E")
+          break;
+        case 83:
+          this.body.turn("S")
+          break;
+        case 65:
+          this.body.turn("W")
+          break;
+      }
     }
-    
+
+    Snake.View.prototype.start = function() {
+      window.setInterval((function() {
+        this.step();
+      }).bind(this), 1000 / 30)
+    }
+
+    Snake.View.prototype.step = function() {
+      this.body.move();
+      this.render();
+    }
+
     Snake.View.prototype.render = function () {
         var theString = "";
-    
+
         for (var i = 0; i < BOARD_SIZE; i++) {
             for (var j = 0; j < BOARD_SIZE; j++) {
                 var matched = false
@@ -28,13 +61,13 @@
                     }
                 }
                 var klass = (matched) ? "bodyPart" : "empty";
-                theString += "<div class='square " + klass + "'></div>"; 
+                theString += "<div class='square " + klass + "'></div>";
             }
         }
         this.$el.html(theString)
     }
-    
-    
+
+
 
 
 })();
