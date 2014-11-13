32d0da018bbf611eb50aea64248edaea02ba3709
Aaron Strick <aaronstrick@gmail.com>
2014-11-13 02:22:23 -0800

    add the ability to eat apples and grow

--- js/snake-view.js
+++ js/snake-view.js
@@ -8,12 +8,19 @@
     var View = Snake.View = function ($el) {
         this.$el = $el;
         this.body = new Snake.Body();
+        this.moveApple();
         this.render();
+        this.apple_count = 0;
+
         this.start();
 
         $(window).on("keydown", this.handleKeyEvent.bind(this));
 }
 
+
+    Snake.View.prototype.moveApple = function() {
+      this.currentApple = [(Math.floor(Math.random() * BOARDSIZE)), (Math.floor(Math.random() * BOARDSIZE))]
+    }
 
     Snake.View.prototype.handleKeyEvent = function(event) {
       switch(event.keyCode) {
@@ -32,6 +39,16 @@
       }
     }
 
+
+    Snake.View.prototype.appleCheck = function() {
+      if (this.body.segments[0].compare(this.currentApple)) {
+        this.moveApple();
+        this.body.grow();
+        this.appleCount += 1;
+
+      }
+    }
+
     Snake.View.prototype.start = function() {
       window.setInterval((function() {
         this.step();
@@ -40,6 +57,7 @@
 
     Snake.View.prototype.step = function() {
       this.body.move();
+      this.appleCheck();
       this.render();
     }
 
@@ -49,13 +67,18 @@
         for (var i = 0; i < BOARDSIZE; i++) {
             for (var j = 0; j < BOARDSIZE; j++) {
                 var matched = false
+                var isApple = false
                 for (var k = 0; k < this.body.segments.length; k++) {
                     if (this.body.segments[k].compare([i, j])) {
                         matched = true;
                     }
+                    if ((this.currentApple[0] == i) && (this.currentApple[1] == j)) {
+                      isApple = true;
+                    }
                 }
                 var klass = (matched) ? "bodyPart" : "empty";
-                theString += "<div class='square " + klass + "'></div>";
+                var apple = (isApple) ? "apple" : "";
+                theString += "<div class='square " + klass + " " + apple + "'></div>";
             }
         }
         this.$el.html(theString)
--- js/snake.js
+++ js/snake.js
@@ -2,26 +2,26 @@
     if (typeof Snake === "undefined") {
         window.Snake = {};
     };
-    
+
     var DIRECTION_COORDS = {
         "N": [-1, 0],
         "E": [0, 1],
         "S": [1, 0],
         "W": [0, -1]
     };
-    
+
      var Body = Snake.Body = function() {
         this.dir = "S";
         this.segments = [new Snake.Coord([0,0]), new Snake.Coord([1,0]), new Snake.Coord([2,0])];
     };
-    
+
     Body.prototype.move = function () {
         var face = this.segments.slice(-1)[0].plus(DIRECTION_COORDS[this.dir])
         this.segments.push(face)
         this.segments.shift()
         return this.segments
     };
-    
+
     Body.prototype.turn = function (newDir) {
         if (DIRECTION_COORDS[this.dir].indexOf(0) == DIRECTION_COORDS[newDir].indexOf(0)) {
             this.dir = this.dir;
@@ -30,6 +30,10 @@
         }
     }
 
+    Body.prototype.grow = function() {
+        var face = this.segments.slice(-1)[0].plus(DIRECTION_COORDS[this.dir])
+        this.segments.push(face)
+    }
 
 
 
