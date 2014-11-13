da4bbf5daf51b079f8dc27e4198ccf6559755b5c
Aaron Strick <aaronstrick@gmail.com>
2014-11-12 17:55:14 -0800

    added board design to html

---
+++ js/snake-view.js
@@ -0,0 +1,35 @@
+(function () {
+    if (typeof Snake === "undefined") {
+        window.Snake = {};
+    }
+    
+    var BOARD_SIZE = 20;
+    
+    var View = Snake.View = function ($el) {
+        this.$el = $el;
+        this.body = new Snake.Body();
+        this.render();
+    }
+    
+    Snake.View.prototype.render = function () {
+        var theString = "";
+    
+        for (var i = 0; i < BOARD_SIZE; i++) {
+            for (var j = 0; j < BOARD_SIZE; j++) {
+                var matched = false
+                for (var k = 0; k < this.body.segments.length; k++) {
+                    if (this.body.segments[k].compare([i, j])) {
+                        matched = true;
+                    }
+                }
+                var klass = (matched) ? "bodyPart" : "empty";
+                theString += "<div class='square " + klass + "'></div>"; 
+            }
+        }
+        this.$el.html(theString)
+    }
+    
+    
+
+
+})();
